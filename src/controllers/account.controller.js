3a8571cee3772e901a231b3e64e97bf6b95d6948
GammaGammaRay <benjamin.peret@gmail.com>
2023-08-05 08:38:36 -0300

    fix: router

---
+++ src/controllers/account.controller.js
@@ -0,0 +1,11 @@
+import { db } from "../database/database.connection"
+
+async function userSignUp() {}
+
+async function userSignIn() {}
+
+async function userGet() {}
+
+async function userRanking() {}
+
+export { userSignUp, userSignIn, userGet, userRanking }
---
+++ src/controllers/urls.controller.js
@@ -0,0 +1,11 @@
+import { db } from "../database/database.connection"
+
+async function urlShorten() {}
+
+async function urlGetById() {}
+
+async function urlRedirectTo() {}
+
+async function urlDelete() {}
+
+export { urlShorten, urlGetById, urlRedirectTo, urlDelete }
